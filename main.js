472752374ec737aef193fdadbc04239f61ae2a25
Grady Robbins <gradyrobbins13@gmail.com>
2019-06-17 16:47:01 -0500

    can console log the song title but .each not working properly

--- main.js
+++ main.js
@@ -13,24 +13,44 @@ console.log(displayContainer.text())
     //     </li>
     // `)
 
+
 //jQuery create Event Handlers
 const load_songs_button = $("#load-songs")
 
-// Attach a click handler to the button with jQuery. When
+// Attach a click handler to the load-songs-button with jQuery.
 load_songs_button.click(function (evt) {
     // console.log(evt.target.id)
+    //all of the below is pg 481 of JavaScript/JQuery the missing manual by D. MacFarland
+    $.getJSON('songs.json',  processContacts);
+        function processContacts(data){
+            // var infoHTML= '';
+            console.log(data.songs[0].title)
+            console.log(data.songs[1].title)
+            console.log(data.songs[2].title)
+            console.log(data.songs[3].title)
 
+            // $.each(data, function(data) {
+            //     console.log(`song :` + `${data}`)
+            // } )
+
+            // $.each(data, function(data){
+            //     console.log(`${data.songs.title}`)
+            //     // infoHTML += `<li class="song" id="next-song">${data.songs.title}</li>`
+            // })
+            // .appendTo("#song-list");
+
+    }
     //  when the button is clicked; add hard coded info onto the DOM
-    $(`
+        // $(`
 
-                <li class="song" id="next-song">
-                    <h1 class="song__title">{Title of song}</h1>
-                    <section class="song__description">
-                        Performed by {artist} on the album {album}
-                    </section>
-                </li>
-    `)
-    .appendTo("#song-list")
+        //             <li class="song" id="next-song">
+        //                 <h1 class="song__title">{Title of song}</h1>
+        //                 <section class="song__description">
+        //                     Performed by {artist} on the album {album}
+        //                 </section>
+        //             </li>
+        // `)
+        // .appendTo("#song-list")
 
     //AJAX: Asynchronous JS and XML
     //use $.ajax() to load `songs.json` from the file system
@@ -80,4 +100,5 @@ $(document).ready(function(){
     $('#empty').click(function(){
         $('#song-list').empty()
     })
+
 })
